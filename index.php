a6d59d1c6eb6dc7d106e81deddeb71a45a721c67
A1093325 <100358083+Sandyhsy@users.noreply.github.com>
2022-03-01 17:10:56 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+$i=10;
+$j=3;
+echo "How are you?";
+echo "</br>";
+echo "<b>$i+$j=</b>";
+echo $i+$j;
+echo "</br>";
+echo date("Y,m,d");
+?> 
+</body>
+</html>
